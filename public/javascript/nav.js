f78b961dc5f18a7d1b452c445c97c656a8225294
Prasanna-13-RV <prasannavelmurugan0200@gmail.com>
2021-11-12 22:45:51 +0530

    changes

--- public/javascript/nav.js
+++ public/javascript/nav.js
@@ -1,3 +1,24 @@
+var mybutton = document.querySelector(".topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 const navToggler = document.querySelector(".nav-toggler");
 const navMenu = document.querySelector(".site-navbar ul");
 const navLinks = document.querySelectorAll(".site-navbar a");
@@ -5,17 +26,17 @@ const navLinks = document.querySelectorAll(".site-navbar a");
 allEventListners();
 
 function allEventListners() {
-    navToggler.addEventListener("click", togglerClick);
-    navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
+  navToggler.addEventListener("click", togglerClick);
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
 }
 
 function togglerClick() {
-    navToggler.classList.toggle("toggler-open");
-    navMenu.classList.toggle("open");
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
 }
 
 function navLinkClick() {
-    if (navMenu.classList.contains("open")) {
-        navToggler.click();
-    }
+  if (navMenu.classList.contains("open")) {
+    navToggler.click();
+  }
 }
